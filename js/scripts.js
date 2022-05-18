7a53afbad0c07817c7c04717ac2176637b419fd7
Uğur Şenaslan <91412306+senaslanugur@users.noreply.github.com>
2022-05-18 16:37:19 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,25 @@
 //    }};
 //
 // xhr.send();
-$.ajax({ url: 'https://cors-anywhere.herokuapp.com/https://podcastrepublic.net', success: function(data) { console.log(data); } });
+var data =""
+$.ajax({ url: 'https://cors-anywhere.herokuapp.com/https://www.podcastrepublic.net',  async:false, success: function(a) {
+  // console.log(data)
+   data = a
+ }
+});
+
+var el = document.createElement( 'html' );
+el.innerHTML = data;
+var span  = el.getElementsByClassName("thumbnail")// Live NodeList of your anchor element
+var caption  = el.getElementsByClassName("caption")
+var urls = el.getElementsByTagName("a")
+console.log(urls)
+
+console.log(span.length)
+var htr = document.getElementById("interface")
+for(var i=12;i<span.length;i++){
+
+  htr.innerHTML += '<div class="row gx-lg-5" > <div class="col-lg-6 col-xxl-4 mb-5" ><div class="card bg-light border-0 h-100" ><div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0" >'+
+                    '<div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">'+ span[i].innerHTML+'</div><h2 class="fs-4 fw-bold">'+caption[i].innerHTML+'</h2>'+
+                    '<p class="mb-0"></p></div></div></div>'
+}
