07cb9bacf73ff40383f91d04e9648728264e6532
Uğur Şenaslan <91412306+senaslanugur@users.noreply.github.com>
2022-05-18 15:02:12 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-$.ajax({ url: 'https://www.podcastrepublic.net/', success: function(data) { console.log(data); } });
+// var url = "www.podcastrepublic.net";
+//
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", url);
+//
+//
+//
+// xhr.onreadystatechange = function () {
+//    if (xhr.readyState === 4) {
+//       console.log(xhr.status);
+//       console.log(xhr.responseText);
+//    }};
+//
+// xhr.send();
+$.ajax({ url: 'https://cors-anywhere.herokuapp.com/https://podcastrepublic.net', success: function(data) { console.log(data); } });
