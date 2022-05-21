42e51a37486d55f477782b65d175ee21244f405e
Uğur Şenaslan <91412306+senaslanugur@users.noreply.github.com>
2022-05-21 15:32:43 +0300

    Update connect.js

--- js/connect.js
+++ js/connect.js
@@ -1,7 +1,7 @@
 function test(){
 
   var main_url = "https://cors-anywhere.herokuapp.com/"
-  var url = "https://cors-anywhere.herokuapp.com/corsdemo?accessRequest=09f2cf0db8fa06a409034124e0a621399d7a91d3de74eabc7377afc5c8cb8c1d"
+  var url = main_url + "https://cors-anywhere.herokuapp.com/corsdemo?accessRequest=09f2cf0db8fa06a409034124e0a621399d7a91d3de74eabc7377afc5c8cb8c1d"
   $.ajax({ url: url,
     headers: {'Access-Control-Allow-Origin': '*' },
     async:false,
