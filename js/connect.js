32227d0f8809a95fb1b483b563ad77846477880e
Uğur Şenaslan <91412306+senaslanugur@users.noreply.github.com>
2022-05-24 22:30:34 +0300

    Update connect.js

--- js/connect.js
+++ js/connect.js
@@ -1,12 +1,11 @@
 function test(){
 
-  var main_url = "https://cors-anywhere.herokuapp.com/"
-  var url = main_url + "https://cors-anywhere.herokuapp.com/corsdemo?accessRequest=09f2cf0db8fa06a409034124e0a621399d7a91d3de74eabc7377afc5c8cb8c1d"
-  $.ajax({ url: url,
-    headers: {'Access-Control-Allow-Origin': '*' },
-    async:false,
-    success: function(a) {
-      location.reload();
-     },
-  });
+  // var main_url = "https://cors-anywhere.herokuapp.com/"
+  var url = "https://cors-anywhere.herokuapp.com/corsdemo?accessRequest=09f2cf0db8fa06a409034124e0a621399d7a91d3de74eabc7377afc5c8cb8c1d"
+  // $.get(url, function(data, status){
+  //   alert("Data: " + data + "\nStatus: " + status);
+  // });
+
+  xhttp.open("POST",url);
+  xhttp.send();
 }
