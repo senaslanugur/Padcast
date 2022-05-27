af7e3dc1575aebdf7f79b3b957628f7ce380344c
Uğur Şenaslan <91412306+senaslanugur@users.noreply.github.com>
2022-05-27 08:07:44 +0300

    Add files via upload

--- js/listing_results.js
+++ js/listing_results.js
@@ -85,3 +85,20 @@ function go_listen(d){
   var podcasts = d.id
   localStorage.setItem("podcasts",podcasts)
 }
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("test");
+    li = ul.getElementsByClassName("mb-5");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("h5")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
